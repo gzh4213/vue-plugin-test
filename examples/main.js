71c4276e2e8a9dbb5023f2f5a651bf347fc1d988
guozhen <guozhen4213@126.com>
2020-06-03 23:19:43 +0800

    toast plugin

--- examples/main.js
+++ examples/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import Toast from '../packages/index'
 Vue.config.productionTip = false
-
+console.log('Toast:',Toast)
+Vue.use(Toast)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
---
+++ packages/toast/src/toast.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="test">
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'toast',
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.test {
+  font-size: 20px;
+  color: red;
+}
+</style>
